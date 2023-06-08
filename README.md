65a68656cb54b571ef1a20558be7e2e0e7967067
RyanOxon <rafael.salgado31@gmail.com>
2023-06-08 14:39:31 -0300

    Implementação do APP 5

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ App 2 > Aplicativo de Pedra Papel e tesoura, tela unica
 App 3 > Aplicativo de uma empresa generica, diversas telas
 
 App 4 > Aplicativo de Cara ou coroa, 2 telas e passagem de informacao entre as telas
+
+App 5 > Aplicativo de Calculadora Hexadecimal, com entradas pela tela ou pelo teclado
--- app5/calc_hexa/lib/home.dart
+++ app5/calc_hexa/lib/home.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,14 +11,11 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final _hexAndOperatorRegex = RegExp(r"[0-9A-Fa-f+-/*]");
   String inputValue = "";
+  bool finish = false;
 
   void _calculate(String value) {
-    inputValue = value
-        .replaceAll("+", " + ")
-        .replaceAll("-", " - ")
-        .replaceAll("*", " * ")
-        .replaceAll("/", " / ");
-
+    finish = true;
+    inputValue = value.trim();
     String result;
     try {
       result = _calculaHexa(inputValue);
@@ -85,52 +81,165 @@ class _HomeState extends State<Home> {
     return resultado;
   }
 
+  ElevatedButton botao(String texto, bool isOperator,
+      {bool isBackspace = false,
+      bool isDouble = false,
+      bool isEquals = false}) {
+    return ElevatedButton(
+        onPressed: () {
+          if (finish) {
+            setState(() {
+              inputValue = "";
+              finish = false;
+            });
+          }
+          if (isEquals && inputValue.isNotEmpty) {
+            _calculate(inputValue);
+          } else if (isBackspace) {
+            setState(() {
+              if (inputValue.isNotEmpty) {
+                inputValue = inputValue.substring(0, inputValue.length - 1);
+                inputValue = inputValue.trim();
+              }
+            });
+          } else if (!isEquals) {
+            setState(() {
+              if (isOperator && inputValue.isNotEmpty) {
+                inputValue += " $texto ";
+              } else {
+                inputValue += texto;
+              }
+            });
+          }
+        },
+        style: ButtonStyle(
+            backgroundColor: isOperator
+                ? const MaterialStatePropertyAll(Colors.red)
+                : const MaterialStatePropertyAll(Colors.blueGrey),
+            elevation: const MaterialStatePropertyAll(2),
+            shape: const MaterialStatePropertyAll(RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)))),
+            fixedSize: MaterialStatePropertyAll(
+                isDouble ? const Size(182, 70) : const Size(90, 70))),
+        child: Text(texto,
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("Calculadora Hexadecimal"),
-            backgroundColor: Colors.green),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(20),
-            child: Column(children: [
-              Container(
-                  margin: const EdgeInsets.all(15),
-                  padding: const EdgeInsets.only(left: 10),
-                  alignment: Alignment.topLeft,
-                  decoration: ShapeDecoration(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      color: Colors.green),
-                  child: Text(inputValue,
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold))),
-              TextField(
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(_hexAndOperatorRegex)
-                ],
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Insira uma expressao Hexadecimal",
-                ),
-                onSubmitted: (value) {
-                  _calculate(value);
-                },
+      appBar: AppBar(
+          title: const Text("Calculadora Hexadecimal"),
+          backgroundColor: Colors.blueGrey),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 245),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Container(
+                constraints: const BoxConstraints(
+                    minHeight: 70,
+                    maxHeight: 140,
+                    minWidth: double.infinity,
+                    maxWidth: double.infinity),
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+                decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    color: Colors.green),
+                child: Text(inputValue,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold))),
+            TextField(
+              keyboardType: TextInputType.text,
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(_hexAndOperatorRegex)
+              ],
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Insira uma expressao Hexadecimal",
               ),
-              Row(children: [
-                ElevatedButton(
-                    onPressed: () {},
-                    style: const ButtonStyle(
-                        fixedSize: MaterialStatePropertyAll(Size.square(50))),
-                    child: const Text("A"))
-              ]),
-              const Row(children: [Text("data")]),
-              const Row(children: [Text("data")]),
-              const Row(children: [Text("data")]),
+              onSubmitted: (value) {
+                _calculate(value);
+              },
+            ),
+            const Padding(padding: EdgeInsets.all(10)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                botao("1", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("2", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("3", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("⟵", true, isBackspace: true)
+              ],
+            ),
+            const Padding(padding: EdgeInsets.all(2)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                botao("4", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("5", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("6", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("+", true)
+              ],
+            ),
+            const Padding(padding: EdgeInsets.all(2)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                botao("7", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("8", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("9", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("-", true)
+              ],
+            ),
+            const Padding(padding: EdgeInsets.all(2)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                botao("A", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("B", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("C", false),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("*", true)
+              ],
+            ),
+            const Padding(padding: EdgeInsets.all(2)),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              botao("D", false),
+              const Padding(padding: EdgeInsets.all(1)),
+              botao("E", false),
+              const Padding(padding: EdgeInsets.all(1)),
+              botao("F", false),
+              const Padding(padding: EdgeInsets.all(1)),
+              botao("/", true)
             ]),
-          ),
-        ));
+            const Padding(padding: EdgeInsets.all(2)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                botao("0", false, isDouble: true),
+                const Padding(padding: EdgeInsets.all(1)),
+                botao("=", true, isDouble: true, isEquals: true)
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
